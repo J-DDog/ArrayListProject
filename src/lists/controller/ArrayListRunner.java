9fed7a205d7f4258c4d4b9419b22b153ca052d32
Traitortots <jker3169@740s-pl-116.local>
2015-10-07 09:32:44 -0600

    Making a new Project and added my start and controller classes

---
+++ src/lists/controller/ArrayListRunner.java
@@ -0,0 +1,12 @@
+package lists.controller;
+
+import lists.controller.ArrayListController;
+
+public class ArrayListRunner
+{
+	public static void main(String[] args)
+	{
+		ArrayListController myController = new ArrayListController();
+		myController.start();
+	}
+}
